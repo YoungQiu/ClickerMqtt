f4119e3d328faec35c641abe3f59cc188ec252ba
Young Qiu <qzy0106@gmail.com>
2011-02-14 10:59:44 +1000

    current file

--- main.php
+++ main.php
@@ -71,10 +71,10 @@
 		/* Jquery for fun */
 		$(Document).ready(function() {
 			$('#room_image_div').fadeOut(500).fadeIn(500);
-			$('#control').delay(500).slideDown(1000);
+			$('#control').delay(1000).slideDown(1000);
 			$('#control_btn').click(function(){
 				$(this).fadeOut();
-				$('#control_div').delay(1000).slideDown(500);	
+				$('#control_div').delay(1000).fadeIn(1000);	
 				$('#table1').fadeIn(1000);
 				$('#table2').fadeIn(1000);
 				$('#table3').fadeIn(1000);
@@ -86,6 +86,18 @@
 				$('#table9').fadeIn(1000);
 				$('#table10').fadeIn(1000);
 				$('#table11').fadeIn(1000);
+				
+				$('#run').click(function() {
+					$(this).hide();
+					$('#answer').fadeIn(500);
+					$('#stop').show();
+				});
+				
+				$('#stop').click(function() {
+					$(this).hide();
+					$('#answer').fadeOut(500);
+					$('#run').show();
+				});
 			});
 		});
 	
@@ -94,7 +106,7 @@
 
 <body>
 
-<div id="room_image_div" style="position:relative; text-align:left; width:960px; margin-left:0px; margin-right:auto;">	
+<div id="room_image_div" style="position:absolute; text-align:left; width:960px; margin-left:0px; margin-right:auto;">	
 <img id="room_image" src="room.png" border="0" width="960" />
 
 <div id="table1" style="display:none"></div>
@@ -113,14 +125,18 @@
 
 <br /><br />
 
-<div id="control" style="display:none; position:absolute; text-align:left; margin-top:30px; margin-left:180px; margin-right:auto;" >
-<input id='control_btn' type='button' value='CONTROLS' style="width:600px" />
+<div id="control" style="display:none; position:absolute; text-align:left; margin-top:620px; margin-left:160px; margin-right:auto;" >
+<input id='control_btn' type='button' value='START' style="width:600px" />
 </div>
 
-<div id="control_div" style="display:none; position:absolute; text-align:left; margin-top:30px; margin-left:180px; margin-right:auto;" >
-<input type='button' onclick='start();' value='START' style="width:200px;" />
-<input type='button' onclick='stop();' value='STOP' style="width:200px;" />
+<div id="control_div" style="display:none; position:absolute; text-align:left; margin-top:620px; margin-left:160px; margin-right:auto;" >
+<input id="run" type='button' onclick='start();' value='RUN' style="width:400px;" />
+<input id="stop" type='button' onclick='stop();' value='STOP' style="display:none; width:400px;" />
 <input type='button' onclick='reset();' value='RESET' style="width:200px;" />
+</div>
+
+<div id="answer" style="display:none; position:absolute; margin-left:800; margin-top:580">
+<p style="color:red"><font size="16"><b>B</b></font></p>
 </div>
 
 </body>
